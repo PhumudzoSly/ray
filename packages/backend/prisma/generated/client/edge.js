34eb6cbb57eb59b924e96aab193857058466dbdb
Phumudzo Mahandana <phumudzo@soimagine.co.za>
2025-07-22 18:24:53 +0200

    UPDATED PRISMA CLIENT

--- packages/backend/prisma/generated/client/edge.js
+++ packages/backend/prisma/generated/client/edge.js
@@ -1366,7 +1366,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- packages/backend/prisma/generated/client/index.js
+++ packages/backend/prisma/generated/client/index.js
@@ -1367,7 +1367,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
